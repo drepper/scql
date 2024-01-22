3e92a4ac4f6b2d70891d7282082d2696145310fd
Ulrich Drepper <drepper@gmail.com>
2024-01-22 20:32:59 +0100

    Various cleanups
    - remove enum Type and use isinstance instead
    - move known_name into Lambda class
    - better variable names
    - add comments and docstrings

--- combinator.py
+++ combinator.py
@@ -1,34 +1,51 @@
+#! /usr/bin/env python3
+# Copyright © 2024 Ulrich Drepper <drepper@akkadia.org>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files(the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 """Evaluation and reduction of lambda expression."""
 from __future__ import annotations
 
 import argparse
-import enum
 import sys
-from typing import ClassVar, Dict, List, override, Tuple
+from typing import ClassVar, Dict, List, override, Tuple, Type
 
 
-def charcnt(s: str, c: str) -> int:
-  return sum(sc == c for sc in s)
-
-
-@enum.unique
-class Type(enum.Enum):
-  EMPTY = 0
-  CONST = 1
-  VAR = 2
-  CALL = 3
-  LAMBDA = 4
-
-
+# These are the characters accepted and used as variable names.  The list
+# could be extended here and everything else should just work.  But it
+# should be noted that
+# a) uppercase characters are used in the names of known combinators
+# b) some greek letters (both lowercase like λ and π and uppercase like
+#    Φ and Ψ
+# are used for non-variables and conflicts would be fatal.
 VARIABLE_NAMES = 'abcdefghijklmnopqrstuvwxyz'
 
 
 class Naming: # pylint: disable=too-few-public-methods
+  """This is an object encapsulating the predictable generation of distinct
+  variable names."""
   def __init__(self):
     self.next = VARIABLE_NAMES[0]
     self.known = {}
 
   def get(self, v: Var) -> str:
+    """Get the next variable name."""
     if v.id not in self.known:
       assert self.next in VARIABLE_NAMES, 'too many variables'
       self.known[v.id] = self.next
@@ -40,6 +57,11 @@ class Naming: # pylint: disable=too-few-public-methods
     return self.known[v.id]
 
 
+# List of combinators, mostly taken from
+#   https://www.angelfire.com/tx4/cus/combinator/birds.html
+# These correspond to the Smullyan's "To Mock a Mockingbird".
+# The list can be extended.  The names are used in translation from the
+# input and when printing the result.
 KNOWN_COMBINATORS = {
   'B': 'λabc.a(bc)',
   'B₁': 'λabcd.a(bcd)',
@@ -85,52 +107,50 @@ KNOWN_COMBINATORS = {
 
 
 def remove_braces(s: str) -> str:
+  """Return argument without enclosing parenthesis or the string itself."""
   return s[1:-1] if s and s[0] == '(' and s[-1] == ')' else s
 
 
-def known_name(la: str) -> str:
-  lar = remove_braces(la)
-  for name, lstr in KNOWN_COMBINATORS.items():
-    if lstr == lar:
-      if args.tracing: # pylint: disable=used-before-assignment
-        print(f'→ {lar}')
-      return name
-  return la
-
-
 class Obj:
-  def __init__(self, t: Type):
-    self.t = t
-
-  def is_a(self, t: Type) -> bool:
-    return self.t == t
+  """Base class for node in the graph representation of a lambda expression."""
+  def is_a(self, oc: Type[Application, Lambda]) -> bool:
+    """Check whether a derived object is of a specific type."""
+    return isinstance(self, oc)
 
   def is_free(self, v: Var) -> bool: # pylint: disable=unused-argument
+    """Test whether this is an object for a free variable.  This is the generic
+    implementation."""
     return False
 
   def __str__(self) -> str:
     raise NotImplementedError('__str__ called for Obj')
 
   def fmt(self, varmap: Naming) -> str:
+    """Format an expression as a string.  This pure virtual version must never
+    be called."""
     raise NotImplementedError('fmt called for Obj')
 
   def replace(self, v: Var, expr: Obj) -> Obj: # pylint: disable=unused-argument
+    """Return the expression with the given variable replaced by the expression."""
     return self
 
   def duplicate(self) -> Obj:
+    """Duplicate the object."""
     return self
 
 
 class Var(Obj):
+  """Object to represent a variable in the lambda expression graph."""
   varcnt: ClassVar[int] = 1
 
   def __init__(self):
-    super().__init__(Type.VAR)
     self.id = Var.varcnt
     Var.varcnt += 1
 
   @override
   def is_free(self, v: Var) -> bool:
+    """Test whether this is a free variable.  If we come here and this is the variable
+    we are looking for it is indeed free."""
     return self.id == v.id
 
   @override
@@ -147,21 +167,20 @@ class Var(Obj):
 
 
 class Empty(Obj):
-  def __init__(self):
-    super().__init__(Type.EMPTY)
-
+  """Object returned to indicate errors when parsing lambda expressions."""
   @override
   def __str__(self):
     return '{}'
 
   @override
   def fmt(self, varmap: Naming) -> str:
+    # This is a filler.  An object of this type should never really be used.
     return '○'
 
 
 class Constant(Obj):
+  """Object to represent constants in the lambda expression graph."""
   def __init__(self, name: str):
-    super().__init__(Type.CONST)
     self.name = name
 
   @override
@@ -174,10 +193,11 @@ class Constant(Obj):
 
 
 class Application(Obj):
+  """Object to represent the application (call) to a function in the lambda
+  expression graph."""
   def __init__(self, ls: List[Obj]):
-    super().__init__(Type.CALL)
     assert len(ls) >= 2
-    self.code = (ls[0].code + ls[1:]) if ls and ls[0].is_a(Type.CALL) else ls
+    self.code = (ls[0].code + ls[1:]) if ls and ls[0].is_a(Application) else ls
     assert self.code
 
   @override
@@ -201,7 +221,10 @@ class Application(Obj):
     return Application([e.duplicate() for e in self.code])
 
   def beta(self) -> Obj:
-    if not self.code[0].is_a(Type.LAMBDA):
+    """Perform beta reduction on the given application.  This is called on a freshly
+    created object but the reduction cannot be performed in the constructor because
+    the result of the beta reduction can be something other than an application."""
+    if not self.code[0].is_a(Lambda):
       return self
     la: Lambda = self.code[0]
     r = la.code.replace(la.params[0], self.code[1])
@@ -211,11 +234,11 @@ class Application(Obj):
 
 
 class Lambda(Obj):
+  """Object to represent a lambda expression in the lambda expression graph."""
   def __init__(self, params: List[Var], ctx: Dict[str, Var], code: Obj):
-    super().__init__(Type.LAMBDA)
     if not params:
       raise SyntaxError('lambda parameter list cannot be empty')
-    if code.is_a(Type.LAMBDA):
+    if code.is_a(Lambda):
       self.params = params + code.params
       self.ctx = dict(ctx, **code.ctx)
       self.code = code.code
@@ -226,7 +249,7 @@ class Lambda(Obj):
 
   @override
   def is_free(self, v: Var) -> bool:
-    return all(e.is_free(v) for e in self.code)
+    return v not in self.params and all(e.is_free(v) for e in self.code)
 
   @override
   def __str__(self):
@@ -236,7 +259,7 @@ class Lambda(Obj):
   def fmt(self, varmap: Naming) -> str:
     # It is important to process the params first to ensure correct naming of parameter variables
     paramstr = ''.join([a.fmt(varmap) for a in self.params])
-    return known_name(f'(λ{paramstr}.{remove_braces(self.code.fmt(varmap))})')
+    return Lambda.known_name(f'(λ{paramstr}.{remove_braces(self.code.fmt(varmap))})')
 
   @override
   def replace(self, v: Var, expr: Obj) -> Obj:
@@ -250,8 +273,23 @@ class Lambda(Obj):
       newcode = newcode.replace(o, n)
     return newlambda(newparams, self.ctx, newcode)
 
+  @staticmethod
+  def known_name(la: str) -> str:
+    """Determine whether the formatted expressions corresponds to a known combinator
+    and return that name.  Otherwise return the original expression."""
+    lar = remove_braces(la)
+    for name, lstr in KNOWN_COMBINATORS.items():
+      if lstr == lar:
+        if args.tracing: # pylint: disable=used-before-assignment
+          print(f'→ {lar}')
+        return name
+    return la
+
 
 def parse_lambda(s: str, ctx: Dict[str, Var]) -> Tuple[Lambda, str]:
+  """Parse the representation of a lambda definition.  Return the graph
+  representation and the remainder of the string not part of the just
+  parsed part."""
   assert s[0] == 'λ'
   s = s[1:].strip()
   params: List[Var] = []
@@ -268,6 +306,9 @@ def parse_lambda(s: str, ctx: Dict[str, Var]) -> Tuple[Lambda, str]:
 
 
 def parse_paren(s: str, ctx: Dict[str, Var]) -> Tuple[Obj, str]:
+  """Parse an expression in parenthesis.  Return the graph
+  representation and the remainder of the string not part of the just
+  parsed part."""
   assert s[0] == '('
   start = 1
   end = 1
@@ -288,6 +329,11 @@ def parse_paren(s: str, ctx: Dict[str, Var]) -> Tuple[Obj, str]:
 
 
 def get_constant(s: str) -> Tuple[Obj, str]:
+  """Parse a constant name.  Unlike a variable, a constant can be longer than a
+  single character and therefore has to be terminated by either of the special
+  characters using the string representation (λ, (, ), or .) or a whitespace.
+  Return the graph representation and the remainder of the string not part of
+  the just parsed part."""
   i = 0
   while i < len(s) and s[i] != ')' and s[i] != '(' and s[i] != '.' and not s[i].isspace() and s[i] != 'λ':
     i += 1
@@ -300,6 +346,9 @@ def get_constant(s: str) -> Tuple[Obj, str]:
 
 
 def parse_one(s: str, ctx: Dict[str, Var]) -> Tuple[Obj, str]:
+  """Toplevel function to parse a string representation of a lambda expression.
+  Return the graph representation and the remainder of the string not part of
+  the just parsed part."""
   match s[0]:
     case 'λ':
       return parse_lambda(s, ctx)
@@ -316,12 +365,19 @@ def parse_one(s: str, ctx: Dict[str, Var]) -> Tuple[Obj, str]:
 
 
 def newlambda(params: List[Var], ctx: Dict[str, Var], code: Obj):
-  if code.is_a(Type.CALL) and len(params) + 1 == len(code.code) and params == code.code[1:] and all(not code.code[0].is_free(e) for e in params):
+  """Create a new lambda expression using the given parametesr and body of code.
+  But the function also performs η-reduction, i.e., it returns just the function
+  expression (first of the application values) in case the resulting lambda would
+  just apply the required parameter to the application value in order."""
+  if code.is_a(Application) and len(params) + 1 == len(code.code) and params == code.code[1:] and all(not code.code[0].is_free(e) for e in params):
     return code.code[0]
   return Lambda(params, ctx, code)
 
 
 def apply(li: List[Obj]) -> Obj:
+  """Create an application expression given the list of objects.  If only a
+  singular expression is given it is returned, no need to wrap it into an
+  application expression."""
   if len(li) < 2:
     res = li[0] if li else Empty()
   else:
@@ -332,6 +388,10 @@ def apply(li: List[Obj]) -> Obj:
 
 
 def parse_top(s: str, ctx: Dict[str, Var]) -> Tuple[Obj, str]:
+  """Parse a string to a lambda expression, taking one part at a time
+  an creating an application expression from the parts.  Return the graph
+  representation and the remainder of the string not part of the just
+  parsed part."""
   s = s.strip()
   res: List[Obj] = []
   while s:
@@ -342,6 +402,10 @@ def parse_top(s: str, ctx: Dict[str, Var]) -> Tuple[Obj, str]:
 
 
 def from_string(s: str) -> Obj:
+  """Parse a string to a lambda expression, taking one part at a time
+  an creating an application expression from the parts.  Return the graph
+  representation.  In case not all of the expression is parsed raise a
+  syntax error exception."""
   expr, ss = parse_top(s, {})
   if ss:
     raise SyntaxError(f'cannot parse {s}: left over {ss}')
@@ -349,10 +413,13 @@ def from_string(s: str) -> Obj:
 
 
 def to_string(expr: Obj) -> str:
+  """Return a string representation for the lambda expression graph."""
   return remove_braces(expr.fmt(Naming())).rstrip()
 
 
 def handle(al: List[str]) -> int:
+  """Loop over given list of strings, parse, simplify, and print the lambda
+  expression."""
   ec = 0
   for a in al:
     print('\u2501' * 48 + '\n' + a)
@@ -366,6 +433,7 @@ def handle(al: List[str]) -> int:
 
 
 def check() -> int:
+  """Sanity checks.  Return error code that is used as the exit code of the process."""
   checks = [
     ['K', 'K'],
     ['S K K', 'I'],
@@ -418,20 +486,21 @@ def check() -> int:
     ['λabcd.MMM abdc', 'λabcd.MMM abdc'],
   ]
   ec = 0
-  for c in checks:
-    res = to_string(from_string(c[0]))
-    if res != c[1]:
-      if c[1] in KNOWN_COMBINATORS:
-        print(f'❌ {c[0]} → {res} but {c[1]} = {KNOWN_COMBINATORS[c[1]]} expected')
+  for [testinput,expected] in checks:
+    res = to_string(from_string(testinput))
+    if res != expected:
+      if expected in KNOWN_COMBINATORS:
+        print(f'❌ {testinput} → {res} but {expected} = {KNOWN_COMBINATORS[expected]} expected')
       else:
-        print(f'❌ {c[0]} → {res} but {c[1]} expected')
+        print(f'❌ {testinput} → {res} but {expected} expected')
       ec = 1
     else:
-      print(f'✅ {c[0]} → {res}')
+      print(f'✅ {testinput} → {res}')
   return ec
 
 
 def main(al: List[str]) -> None:
+  """Called as main function of the program."""
   if args.check:
     # Overwrite eventual user setting
     args.tracing = False
